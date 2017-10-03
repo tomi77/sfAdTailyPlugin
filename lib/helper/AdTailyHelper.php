c64f57ebd3d46f4a1aaded8f7f448ed31e89185e
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-10-03 20:56:38 +0200

    StyleCI fixes

--- lib/helper/AdTailyHelper.php
+++ lib/helper/AdTailyHelper.php
@@ -2,12 +2,12 @@
 
 function at_insert_ad_taily_ad()
 {
-	if (($code = sfConfig::get('app_ad_taily_plugin_code')) === null) {
-		sfContext::getInstance()->getLogger()->alert('{sfAdTailyPlugin} AdTaily Code is not set!');
-		return;
-	}
+    if (($code = sfConfig::get('app_ad_taily_plugin_code')) === null) {
+        sfContext::getInstance()->getLogger()->alert('{sfAdTailyPlugin} AdTaily Code is not set!');
+        return;
+    }
 
-	if (sfConfig::get('app_ad_taily_plugin_enabled') !== false && $code !== null) {
-		echo str_replace('{$code}', $code, sfConfig::get('app_ad_taily_plugin_template', ''));
-	}
+    if (sfConfig::get('app_ad_taily_plugin_enabled') !== false && $code !== null) {
+        echo str_replace('{$code}', $code, sfConfig::get('app_ad_taily_plugin_template', ''));
+    }
 }
